08038520037809cb90a7f1fcebadad362812c293
younes-dro <dro66_8@hotmail.fr>
2020-11-13 10:35:23 +0100

    Add search on keyup

--- assets/js/vg.js
+++ assets/js/vg.js
@@ -20,14 +20,18 @@
                 layoutMode: 'vertical'
             });
             function onArrange() {
-                $("body,html").animate(
-                        {
-                            scrollTop: $("div.menu-card-wrapper").offset().top
-                        },
-                500, function () {
-                    $('div.menu-card-wrapper').addClass('onarrange');
-                }
-                );
+//                if (!$('div.button-group').hasClass('sticky-menu-nav')) {
+//                    $('.button-group').addClass('sticky-menu-nav');
+//                }
+//
+//                $("body,html").animate(
+//                        {
+//                            scrollTop: $("div.menu-card-wrapper").offset().top
+//                        },
+//                500, function () {
+//                    $('div.menu-card-wrapper').addClass('onarrange');
+//                }
+//                );
             }
             // bind event listener
             iso.on('arrangeComplete', onArrange);
@@ -64,7 +68,7 @@
                     buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
                     event.target.classList.add('is-checked');
                 });
-            }           
+            }
         });
 
 
@@ -102,6 +106,7 @@
         $('div.button-group button').on('click', function () {
 
             if ($('div.button-group').hasClass('sticky-menu-nav')) {
+                $('.button-group').toggleClass('closed');
                 $("body,html").animate(
                         {
                             scrollTop: $("div.menu-card-wrapper").offset().top
@@ -133,6 +138,78 @@
             $linkTo = $(this).attr('href');
             console.log($linkTo);
             $($linkTo + 'Form').modal("show");
+        });
+        var $firstSearch = false;
+        /* Search Form */
+        $('form.search-form').on('submit',function(e){
+            e.preventDefault();
+            return false;
+        });
+        $('input.quicksearch').keyup(function(){
+            $firstSearch = true;
+            
+//            e.preventDefault();
+            if ($('div.search-result')) {
+                $('div.search-result').remove();
+            }
+            var $searchResultContainer = $("<div/>", {"class": "search-result container"});
+            var $searchResult = $("<div/>", {"class": " row"});
+            var $searchClose = $("<span/>", {"class": "ionicon ion-ios-close-outline"});
+
+            var $html = '';
+            $term = $.trim($('#myInput').val());
+            $filter = $term.toUpperCase();
+            console.log($filter);
+            $('span.aliment-title').each(function (index) {
+                $txtValue = $(this).text();
+                if ($txtValue.toUpperCase().indexOf($filter) > -1) {
+                    $html += $(this).parent().closest('div.desc')[0].outerHTML;
+                }
+            });
+            console.log($firstSearch);
+            if($firstSearch){
+                $searchResultContainer.css("animation","none");
+            }
+            $searchResultContainer.append($searchResult.html($html));
+            $('header.site-header').append($searchResultContainer);
+            $searchResultContainer.prepend($searchClose);
+            $('body').addClass('wp_mm_enable');
+        });
+        $('form.search-form a.btn').on('click', function (e) {
+            e.preventDefault();
+            if ($('div.search-result')) {
+                $('div.search-result').remove();
+            }
+            var $searchResultContainer = $("<div/>", {"class": "search-result container"});
+            var $searchResult = $("<div/>", {"class": " row"});
+            var $searchClose = $("<span/>", {"class": "ionicon ion-ios-close-outline"});
+
+            var $html = '';
+            $term = $.trim($('#myInput').val());
+            $filter = $term.toUpperCase();
+            console.log($filter);
+            $('span.aliment-title').each(function (index) {
+                $txtValue = $(this).text();
+                if ($txtValue.toUpperCase().indexOf($filter) > -1) {
+                    $html += $(this).parent().closest('div.desc')[0].outerHTML;
+                }
+            });
+            $searchResultContainer.append($searchResult.html($html));
+            $('header.site-header').append($searchResultContainer);
+            $searchResultContainer.prepend($searchClose);
+            $('body').addClass('wp_mm_enable');
+
+        });
+        $('body').on('click', 'span.ion-ios-close-outline', function () {
+            $('#myInput').val('');
+            $(this).parent().fadeOut('fast');
+            $('body').removeClass('wp_mm_enable');
+        });
+        $(window).keydown(function (event) {
+            if ((event.keyCode == 13)) {
+                event.preventDefault();
+                return false;
+            }
         });
     });
 
